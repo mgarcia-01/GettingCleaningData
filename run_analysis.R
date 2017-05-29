c1acaa00eea322bf58617cac238f255c70b0c1d3
Michael Garcia <mgarcia4703@gmail.com>
2017-05-29 03:02:08 -0700

    updated r_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -15,18 +15,34 @@ XtestURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/
 XtrainURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/X_train.txt"
 YtestURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/test/Y_test.txt"
 YtrainURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/Y_train.txt"
-  
-  
+SubTest <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/subject_test.txt"
+SubTrain <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/subject_train.txt"  
+featuresURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/features.txt"  
 run_analysis(Xtest, Xtrain, Ytest, Ytrain ){
-  Xtest <- read.table(XtestURL)
-  Xtrain <- read.table(XtrainURL)
-  Ytest <- read.table(YtestURL)
-  Ytrain <- read.table(YtrainURL)
- # replaced cbind() with rbind()
-  XSets <- rbind(Xtrain,Xtest)
-  YSets <- rbind(Ytrain,Ytest)
   
-}
+    ### 1. merged data and training sets create one set ##
+      Xtest <- read.table(XtestURL)
+      Xtrain <- read.table(XtrainURL)
+      Ytest <- read.table(YtestURL)
+      Ytrain <- read.table(YtrainURL)
+      features <- read.table(featuresURL)
+     # replaced cbind() with 
+       #TrainSets <- cbind(cbind(Xtrain,Ytrain),SubTrain)
+       #TestSets <- cbind(cbind(Xtest,Ytest),SubTest)
+      XSets <- rbind(Xtrain,Xtest)
+      YSets <- rbind(Ytrain,Ytest)
+    ### 2. Extracts only the measurements on the mean and standard deviation for each measurement.###
+      #need to rename to get measurements 
+       XSetsNames <- features
+      # renamed the xsets to the vector 2 of features as a list
+       names(XSets) <- XSetsNames[,2]
+       mean_std <- grep("(mean|std)\\(\\)", names(XSets))
+      # now create variable for only mean and std dev. 
+        mean_stdSet <- XSets[, mean_std]
+    ### 3. Uses descriptive activity names to name the activities in the data set ###
+        
+        
+  }
 
 
 
