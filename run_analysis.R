6faf2aad15c0852ce5801af259776984044d981e
Michael Garcia <mgarcia4703@gmail.com>
2017-05-29 21:36:13 -0700

    new updates

--- run_analysis.R
+++ run_analysis.R
@@ -15,7 +15,7 @@ XtestURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/
 XtrainURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/X_train.txt"
 YtestURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/test/Y_test.txt"
 YtrainURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/Y_train.txt"
-SubTest <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/subject_test.txt"
+SubTest <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/test/subject_test.txt"
 SubTrain <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/train/subject_train.txt"  
 featuresURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/features.txt"  
 activityURL <- "/Users/michaelgarcia/CloudStation/data_toolbox/GettingCleaningData/UCI HAR Dataset/activity_labels.txt"  
@@ -28,31 +28,51 @@ run_analysis(Xtest, Xtrain, Ytest, Ytrain ){
       Ytrain <- read.table(YtrainURL)
       features <- read.table(featuresURL)
       activityLabels <- read.table(activityURL)
+      subTrain <- read.table(SubTrain)
+      subTest <- read.table(SubTest)
+      
       
      # replaced cbind() with 
        #TrainSets <- cbind(cbind(Xtrain,Ytrain),SubTrain)
        #TestSets <- cbind(cbind(Xtest,Ytest),SubTest)
       XSets <- rbind(Xtrain,Xtest)
       YSets <- rbind(Ytrain,Ytest)[, 1]
+      subjectSets <- rbind(subTrain, subTest)[, 1]
     ### 2. Extracts only the measurements on the mean and standard deviation for each measurement.###
       #need to rename to get measurements 
        XSetsNames <- features
       # renamed the xsets to the vector 2 of features as a list
        names(XSets) <- XSetsNames[,2]
-       mean_std <- grep("(mean|std)\\(\\)", names(XSets))
+       mean_std <- grep("(mean|std)\\(\\)"
+                        , names(XSets)
+                        )
       # now create variable for only mean and std dev. 
         mean_stdSet <- XSets[, mean_std]
     ### 3. Uses descriptive activity names to name the activities in the data set ###
         #YSetsNames <- activityLabels
         #list the elements in vector 2 for the names of activities
-        activityrelabel <- as.character(activityLabels[,2])
+        activityrelabel <- as.character(activityLabels[,2]
+                                        )
+          
         activity <- activityrelabel[YSets]
     ### 4. Appropriately labels the data set with descriptive variable names.
-        
-    ### 5. From the data set in step 4, creates a second, independent tidy data set with the average
-    ###    of each variable for each activity and each subject.
-        
-        
+        #using gsub to replace occurances of string pattern 
+        #See features_info.txt for variable names and descriptions
+            newMeanName <- gsub("-mean\\(\\)", "Mean", names(mean_stdSet), ignore.case=FALSE
+                                )
+            names(mean_stdSet) <- newMeanName
+            newStdDevName <- gsub("-std\\(\\)", "StdDev", names(mean_stdSet), ignore.case=FALSE
+                                       )
+            names(mean_stdSet) <- newStdDevName
+            timeNames <- gsub("^t", "Time", names(mean_stdSet), ignore.case=FALSE
+                                       )
+            names(mean_stdSet) <- timeNames
+            newFreqName <- gsub("^f", "Frequency", names(mean_stdSet), ignore.case=FALSE
+                                )
+            names(mean_stdSet)  <- newFreqName
+            
+          #need to add the subject columns to this set of data
+            
   }
 
 
