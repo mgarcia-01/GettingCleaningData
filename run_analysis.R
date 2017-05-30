a4a9a7721243bd5cbe48bb46704ca629c076d189
Michael Garcia <mgarcia4703@gmail.com>
2017-05-29 23:39:54 -0700

    fixed labeling bc it was late in script

--- run_analysis.R
+++ run_analysis.R
@@ -32,29 +32,49 @@ run_analysis(Xtest, Xtrain, Ytest, Ytrain ){
       subTest <- read.table(SubTest)
       
       
-     # replaced cbind() with 
+     # bind rows. row count the same
        #TrainSets <- cbind(cbind(Xtrain,Ytrain),SubTrain)
        #TestSets <- cbind(cbind(Xtest,Ytest),SubTest)
-      XSets <- rbind(Xtrain,Xtest)
-      YSets <- rbind(Ytrain,Ytest)[, 1]
-      subjectSets <- rbind(subTrain, subTest)[, 1]
+              #XSets <- cbind(Xtrain,Ytrain,subTrain)
+        XSetsNames <- features
+      names(Xtrain) <- XSetsNames[,2]
+      names(Xtest) <- XSetsNames[,2]
+     ## (a) added names to activities 
+      Ytrain <- replace(Ytrain,Ytrain[1] == 1,"WALKING")
+      Ytrain <- replace(Ytrain,Ytrain == 2,"WALKING_UPSTAIRS")
+      Ytrain <- replace(Ytrain,Ytrain == 3,"WALKING_DOWNSTAIRS")
+      Ytrain <- replace(Ytrain,Ytrain == 4,"SITTING")
+      Ytrain <- replace(Ytrain,Ytrain == 5,"STANDING")
+      Ytrain <- replace(Ytrain,Ytrain == 6,"LAYING")
+     
+     names(Ytrain) <- "Activity"
+     names(Ytest) <- "Activity"
+     names(subTrain) <- "Subject"
+     names(subTest) <- "Subject"
+      
+      
+      XSets <- cbind(Xtrain,Ytrain,subTrain)
+      YSets <- cbind(Xtest,Ytest,subTest)
+      CompleteSets <- rbind(XSets, YSets)
+     #NotNEEDED label_feat   <- cbind(features,activityLabels)
+      
     ### 2. Extracts only the measurements on the mean and standard deviation for each measurement.###
       #need to rename to get measurements 
-       XSetsNames <- features
+      
       # renamed the xsets to the vector 2 of features as a list
-       names(XSets) <- XSetsNames[,2]
-       mean_std <- grep("(mean|std)\\(\\)"
-                        , names(XSets)
+       #names(XSets) <- XSetsNames[,2]
+       mean_std <- grep(("(mean|std)\\(\\)")
+                        , names(CompleteSets)
                         )
+    
       # now create variable for only mean and std dev. 
-        mean_stdSet <- XSets[, mean_std]
+        mean_stdSet <- CompleteSets[, mean_std]
+        mean_stdSet <-cbind(mean_stdSet,CompleteSets[562:563])
+        
     ### 3. Uses descriptive activity names to name the activities in the data set ###
-        #YSetsNames <- activityLabels
         #list the elements in vector 2 for the names of activities
-        activityrelabel <- as.character(activityLabels[,2]
-                                        )
-          
-        activity <- activityrelabel[YSets]
+       ####SEE note (a) line 42. Renamed in compilation of set
+       
     ### 4. Appropriately labels the data set with descriptive variable names.
         #using gsub to replace occurances of string pattern 
         #See features_info.txt for variable names and descriptions
@@ -70,6 +90,7 @@ run_analysis(Xtest, Xtrain, Ytest, Ytrain ){
             newFreqName <- gsub("^f", "Frequency", names(mean_stdSet), ignore.case=FALSE
                                 )
             names(mean_stdSet)  <- newFreqName
+            
             
           #need to add the subject columns to this set of data
             
